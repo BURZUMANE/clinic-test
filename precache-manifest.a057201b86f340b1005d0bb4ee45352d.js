4ece85d10510b08f7df1ad842225acdfad37d665
Timur Zakirov <timurzakirov@Timurs-MacBook-Air.local>
2020-08-13 01:51:33 +0300

    Updates

---
+++ precache-manifest.a057201b86f340b1005d0bb4ee45352d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7bbe23cca2f03f4a4f8a85958e45bc",
+    "url": "/clinic-test/index.html"
+  },
+  {
+    "revision": "ee5ac9740cc7d2ce788e",
+    "url": "/clinic-test/static/css/main.19ffea7e.chunk.css"
+  },
+  {
+    "revision": "6e6eaff34d7b0d3fddb3",
+    "url": "/clinic-test/static/js/2.773503f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clinic-test/static/js/2.773503f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5ac9740cc7d2ce788e",
+    "url": "/clinic-test/static/js/main.98e8f57e.chunk.js"
+  },
+  {
+    "revision": "317a109012b2f2d13fe4",
+    "url": "/clinic-test/static/js/runtime-main.8cdf6a7e.js"
+  },
+  {
+    "revision": "3036ae86f3e196e33d3be155da20f495",
+    "url": "/clinic-test/static/media/HelveticaNeueCyr-Thin.3036ae86.otf"
+  },
+  {
+    "revision": "9f79788b7d38ec9e7b69245333c726d1",
+    "url": "/clinic-test/static/media/MaskGroup.9f79788b.jpg"
+  },
+  {
+    "revision": "ac54ae3f47241ba1e4c5a5d01c4a27f0",
+    "url": "/clinic-test/static/media/PFDinTextCondPro-Regular.ac54ae3f.ttf"
+  },
+  {
+    "revision": "d55bae347012ab0273be04250b47dd26",
+    "url": "/clinic-test/static/media/Star.d55bae34.svg"
+  },
+  {
+    "revision": "664f2af37be977122f3f90215ebb1659",
+    "url": "/clinic-test/static/media/header.664f2af3.jpg"
+  },
+  {
+    "revision": "c4f4e9d8042816f88010fb20ecb3e393",
+    "url": "/clinic-test/static/media/header.c4f4e9d8.png"
+  },
+  {
+    "revision": "5b341504bee2994b6d9b743082c7ec5b",
+    "url": "/clinic-test/static/media/image4.5b341504.png"
+  },
+  {
+    "revision": "10584fed048ba48b306b506b239daaf2",
+    "url": "/clinic-test/static/media/lato-bold.10584fed.woff2"
+  },
+  {
+    "revision": "7deb2cbbf9d14692b5d9e6e186ad1b76",
+    "url": "/clinic-test/static/media/lato-bold.7deb2cbb.woff"
+  },
+  {
+    "revision": "bb5842fdfdbd4307e6e7caf6d1068143",
+    "url": "/clinic-test/static/media/lato-bold.bb5842fd.eot"
+  },
+  {
+    "revision": "1cbb2ea06bc8881c0c3c42039fb5324d",
+    "url": "/clinic-test/static/media/lato-regular.1cbb2ea0.eot"
+  },
+  {
+    "revision": "428871b7ec8f7424c3b024c578dc2724",
+    "url": "/clinic-test/static/media/lato-regular.428871b7.woff"
+  },
+  {
+    "revision": "56f135a57b5981a830acc3d192a668da",
+    "url": "/clinic-test/static/media/lato-regular.56f135a5.woff2"
+  },
+  {
+    "revision": "3a9ddf54ae6edbd88ac3022402b3d18f",
+    "url": "/clinic-test/static/media/neuro-image.3a9ddf54.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/clinic-test/precache-manifest.a057201b86f340b1005d0bb4ee45352d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clinic-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
